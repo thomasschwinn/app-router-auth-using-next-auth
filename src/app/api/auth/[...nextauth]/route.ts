a98005f0fc2b3fa6c2c539d10148e841e59d3d1a
thomasschwinn <thomas.schwinn@koeln.de>
2024-01-31 17:25:00 +0100

    initial

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+//import EmailProvider from "next-auth/providers/email";
 
 export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-  ],
+	providers: [
+		// EmailProvider({
+		// 	server: process.env.EMAIL_SERVER,
+		// 	from: process.env.EMAIL_FROM,
+		// 	// maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+		// }),
+		GitHubProvider({
+			clientId: process.env.GITHUB_ID ?? "",
+			clientSecret: process.env.GITHUB_SECRET ?? "",
+		}),
+	],
 };
 
 export const handler = NextAuth(authOptions);
--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -2,16 +2,31 @@ import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
 export default async function ProtectedRoute() {
-  const session = await getServerSession();
-  if (!session || !session.user) {
-    redirect("/api/auth/signin");
-  }
+	const session = await getServerSession();
+	if (!session || !session.user) {
+		redirect("/api/auth/signin");
+	}
+	console.log(session.user);
 
-  return (
-    <div>
-      This is a protected route.
-      <br />
-      You will only see this if you are authenticated.
-    </div>
-  );
+	const allowedUserStr = process.env.allowed_user_list;
+	const allowedUser = JSON.parse(allowedUserStr);
+
+	if (!allowedUser.includes(session.user.name)) {
+		return (
+			<div>
+				This is a protected route.
+				<br />
+				but the user is unknown, thats why you see this message
+			</div>
+		);
+	}
+
+	console.log(allowedUser);
+	return (
+		<div>
+			This is a protected route.
+			<br />
+			You will only see this if you are authenticated.
+		</div>
+	);
 }
